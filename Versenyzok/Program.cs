4103ae2864a8b620a4e0290d60cb6c7006278402
bekesi142 <bekesibalint2006@gmail.com>
2023-05-19 11:03:19 +0200

    utolso push

--- Versenyzok/Program.cs
+++ Versenyzok/Program.cs
@@ -6,10 +6,12 @@ List<string> szuletesi_datum_lista = new List<string>();
 List<string> nemzetisege_lista = new List<string>();
 List<string> rajtszama_lista = new List<string>();
 
+int hanysorvan = 0;
 
 while (!sr.EndOfStream)
 {
     string sor = sr.ReadLine();
+    hanysorvan++;
     string nev = sor.Split(",")[0];
     string szuletesi_datum = sor.Split(",")[1];
     string nemzetisege = sor.Split(",")[2];
@@ -18,8 +20,15 @@ while (!sr.EndOfStream)
     szuletesi_datum_lista.Add(szuletesi_datum);
     nemzetisege_lista.Add(nemzetisege);
     rajtszama_lista.Add(rajtszama);
+    string utolsoember = neve_lista[-1];
+    Convert.ToInt32(rajtszama);
+    int legkisebb_rajtszam = Math.Min(rajtszama_lista.IndexOf);
+    string legkisebb_rajtszam_nemzetisege = nemzetisege_lista[legkisebb_rajtszam];
 }
 
+Console.WriteLine("3. feladat:  " + hanysorvan);
+Console.WriteLine("4. feladat:  " + utolsoember);
+Console.WriteLine("6. feladat:  " + legkisebb_rajtszam_nemzetisege);
 
 
 
