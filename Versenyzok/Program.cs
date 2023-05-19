688dfaa1ef40ac91314d57108fb75a76b5a65501
bekesi142 <bekesibalint2006@gmail.com>
2023-05-19 10:35:26 +0200

    10:35

--- Versenyzok/Program.cs
+++ Versenyzok/Program.cs
@@ -1,13 +1,28 @@
 ﻿StreamReader sr = new StreamReader("pilotak.csv");
 
 
-List<string> neve = new List<string>();
-List<string> szuletesi_datum = new List<string>();
-List<string> nemzetisege = new List<string>();
-List<int> rajtszama = new List<int>();
+List<string> neve_lista = new List<string>();
+List<string> szuletesi_datum_lista = new List<string>();
+List<string> nemzetisege_lista = new List<string>();
+List<string> rajtszama_lista = new List<string>();
 
 
 while (!sr.EndOfStream)
 {
-    string nev = sr.ReadLine();
+    string sor = sr.ReadLine();
+    string nev = sor.Split(",")[0];
+    string szuletesi_datum = sor.Split(",")[1];
+    string nemzetisege = sor.Split(",")[2];
+    string rajtszama = sor.Split(",")[3];
+    neve_lista.Add(nev);
+    szuletesi_datum_lista.Add(szuletesi_datum);
+    nemzetisege_lista.Add(nemzetisege);
+    rajtszama_lista.Add(rajtszama);
 }
+
+
+
+
+
+
+sr.Close();
